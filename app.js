f95452261ed49d629ed9e22a4083a616e3affb02
Sahil Nenwani <sk9941274@gmail.com>
2021-07-16 15:02:15 +0500

    now fully authenticate

--- app.js
+++ app.js
@@ -12,6 +12,69 @@ const regContact=document.getElementById("register-number")
 
 
 
+var registerData=()=>{
+    let key = firebase.database().ref().push().key;
+    var firebasedataofreg={
+        name:regname.value,
+        email:regemail.value,
+        Contact:regContact.value,
+        key:key
+    }
+    document.write(firebasedataofreg)
+    firebase.database().ref("User-Account/"+key).set(firebasedataofreg);
+    // firebase.database.ref()
+
+    firebase.auth().createUserWithEmailAndPassword(regemail.value, regpass.value)
+    .then((result) => { 
+      console.log("succesfuly registerd")
+      console.log(result)
+      
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage)
+      
+    });
+}
+
+var loginData=()=>{
+  
+    firebase.auth().signInWithEmailAndPassword(loginemail.value, loginpass.value)
+    .then((result) => {
+      console.log("successfuly logined")
+     setTimeout(() => {
+      var newpath=  "https://sahilnenwani.github.io/Mode-changer/"
+      return window.location.replace(newpath); 
+     }, 2000);
+      
+      // const Toast = Swal.mixin({
+      //   toast: true,
+      //   position: 'top-end',
+      //   showConfirmButton: false,
+      //   timer: 3000,
+      //   timerProgressBar: true,
+      //   didOpen: (toast) => {
+      //     toast.addEventListener('mouseenter', Swal.stopTimer)
+      //     toast.addEventListener('mouseleave', Swal.resumeTimer)
+      //   }
+      // })
+      
+      // Toast.fire({
+      //   icon: 'success',
+      //   title: 'Signed in successfully'
+      // })
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage)
+      console.log("any error occured")
+    });
+    loginemail.value=""
+    loginpass.value=""
+    
+}
 
 let register=()=>{
     btnexchange.style.left="110px";
@@ -23,18 +86,22 @@ let login=()=>{
     loginbtn.classList.remove('hide');
     registerbtn.classList.add('hide')
 
-    firebase.auth().createUserWithEmailAndPassword(loginemail.value, loginpass.value)
-  .then((result) => { 
-    console.log("succesfuly logined")
     
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorMessage)
-    
-  });
 }
 
-console.log(firebase.auth)
-console.log(firebase.database)
+      // const Toast = Swal.mixin({
+      //   toast: true,
+      //   position: 'top-end',
+      //   showConfirmButton: false,
+      //   timer: 3000,
+      //   timerProgressBar: true,
+      //   didOpen: (toast) => {
+      //     toast.addEventListener('mouseenter', Swal.stopTimer)
+      //     toast.addEventListener('mouseleave', Swal.resumeTimer)
+      //   }
+      // })
+      
+      // Toast.fire({
+      //   icon: 'success',
+      //   title: 'Signed in successfully'
+      // })
