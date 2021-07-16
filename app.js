656782415c603039938dea66b1eee2161711c8f8
Sahil Nenwani <sk9941274@gmail.com>
2021-07-16 23:38:08 +0500

    making code more better

--- app.js
+++ app.js
@@ -9,72 +9,77 @@ const regpass=document.getElementById("register-password")
 const regname=document.getElementById("register-Name")
 const regContact=document.getElementById("register-number")
 
+const regDatabtn=document.getElementById("registerbtn")
+const loginuserbtn=document.getElementById("loginbtn")
 
 
+regDatabtn.addEventListener('click',()=>{
+  event.preventDefault();
+  let key = firebase.database().ref().push().key;
+  var firebasedataofreg={
+      name:regname.value,
+      email:regemail.value,
+      Contact:regContact.value,
+      key:key
+  }
+ 
+  firebase.database().ref("User-Account/"+key).set(firebasedataofreg);
+  // firebase.database.ref()
 
-var registerData=()=>{
-    let key = firebase.database().ref().push().key;
-    var firebasedataofreg={
-        name:regname.value,
-        email:regemail.value,
-        Contact:regContact.value,
-        key:key
-    }
-   
-    firebase.database().ref("User-Account/"+key).set(firebasedataofreg);
-    // firebase.database.ref()
-
-    firebase.auth().createUserWithEmailAndPassword(regemail.value, regpass.value)
-    .then((result) => { 
-      console.log("succesfuly registerd")
-      console.log(result)
-      
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorMessage)
-      
-    });
-}
-
-var loginData=()=>{
-  
-    firebase.auth().signInWithEmailAndPassword(loginemail.value, loginpass.value)
-    .then((result) => {
-      console.log("successfuly logined")
-     setTimeout(() => {
-      var newpath=  "https://sahilnenwani.github.io/Mode-changer/"
-      return window.location.replace(newpath); 
-     }, 2000);
-      
-      // const Toast = Swal.mixin({
-      //   toast: true,
-      //   position: 'top-end',
-      //   showConfirmButton: false,
-      //   timer: 3000,
-      //   timerProgressBar: true,
-      //   didOpen: (toast) => {
-      //     toast.addEventListener('mouseenter', Swal.stopTimer)
-      //     toast.addEventListener('mouseleave', Swal.resumeTimer)
-      //   }
-      // })
-      
-      // Toast.fire({
-      //   icon: 'success',
-      //   title: 'Signed in successfully'
-      // })
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorMessage)
-      console.log("any error occured")
-    });
-    loginemail.value=""
-    loginpass.value=""
+  firebase.auth().createUserWithEmailAndPassword(regemail.value, regpass.value)
+  .then((result) => { 
+    // console.log("succesfuly registerd")
+    // console.log(result)
     
-}
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // console.log(errorMessage)
+    
+  });
+  regname.value="";
+  regemail.value="";
+  regContact.value="";
+  regpass.value="";
+})
+
+loginuserbtn.addEventListener('click',()=>{
+  event.preventDefault();
+  firebase.auth().signInWithEmailAndPassword(loginemail.value, loginpass.value)
+  .then((result) => {
+    
+   setTimeout(() => {
+    var newpath=  "https://sahilnenwani.github.io/Mode-changer/"
+    return window.location.replace(newpath); 
+   }, 2000);
+    
+    // const Toast = Swal.mixin({
+    //   toast: true,
+    //   position: 'top-end',
+    //   showConfirmButton: false,
+    //   timer: 3000,
+    //   timerProgressBar: true,
+    //   didOpen: (toast) => {
+    //     toast.addEventListener('mouseenter', Swal.stopTimer)
+    //     toast.addEventListener('mouseleave', Swal.resumeTimer)
+    //   }
+    // })
+    
+    // Toast.fire({
+    //   icon: 'success',
+    //   title: 'Signed in successfully'
+    // })
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+  loginemail.value=""
+  loginpass.value=""
+
+})
+      
 
 let register=()=>{
     btnexchange.style.left="110px";
@@ -88,20 +93,4 @@ let login=()=>{
 
     
 }
-
-      // const Toast = Swal.mixin({
-      //   toast: true,
-      //   position: 'top-end',
-      //   showConfirmButton: false,
-      //   timer: 3000,
-      //   timerProgressBar: true,
-      //   didOpen: (toast) => {
-      //     toast.addEventListener('mouseenter', Swal.stopTimer)
-      //     toast.addEventListener('mouseleave', Swal.resumeTimer)
-      //   }
-      // })
-      
-      // Toast.fire({
-      //   icon: 'success',
-      //   title: 'Signed in successfully'
-      // })
+  
